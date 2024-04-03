7d69c407e0bf5ff36eb300fa03bfe4b85b562248
ductaihcmit <111119722+ductaihcmit@users.noreply.github.com>
2024-04-03 21:12:08 +0700

    Update nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -23,14 +23,14 @@ export default defineNuxtConfig({
       process.env.NODE_ENV === "development" ? "/about-us/_nuxt/" : "",
     // pageTransition: { name: 'page', mode: 'out-in' }
   },
-  // nitro: {
-  //   runtimeConfig: {
-  //     app: {
-  //       baseURL: "/about-us",
-  //       buildAssetsDir: "_nuxt",
-  //     },
-  //   },
-  // },
+  nitro: {
+    runtimeConfig: {
+      app: {
+        baseURL: process.env.NODE_ENV === "development" ? "/about-us" : "",
+        buildAssetsDir: process.env.NODE_ENV === "development" ? "/_nuxt" : "",
+      },
+    },
+  },
   css: ["ant-design-vue/dist/reset.css", "~/assets/css/main.css"],
   postcss: {
     plugins: {
